fb6818b0df9f112f7f6b16b601ba580964671009
Sabas <sabasjimenez@gmail.com>
2020-03-24 18:46:57 -0600

    Update Basic_library_TTN.ino

--- examples/Basic_library_TTN/Basic_library_TTN.ino
+++ examples/Basic_library_TTN/Basic_library_TTN.ino
@@ -9,7 +9,7 @@ const char *appKey = "00000000000000000000000000000000";
 #define debugSerial Serial
 
 // Replace REPLACE_ME with TTN_FP_EU868 or TTN_FP_US915
-#define freqPlan REPLACE_ME
+#define freqPlan TTN_FP_US915
 
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
 CayenneLPP lpp(51);
